1ec08933b190ff6811d8f2b3099b836e5cc63a50
Chidera Ozoh <Chideraozoh@icloud.com>
2022-10-05 15:33:33 +0100

    Initial commit

---
+++ ETL/Lambda/weather_pollution/etl_airvisual.py
@@ -0,0 +1,10 @@
+# TO DO:
+# Retrieve states for country United Kingdom
+# Retrieve cities in each state
+# Collect weather and pollution data of each cities
+# Using lambda send data to s3 bucket
+# Build data model for data collected
+# Deploy to warehouse Redshift
+# Make process modular
+#
+
